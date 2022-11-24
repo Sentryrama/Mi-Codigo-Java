7bcbf9dc05f5b5050c014b6ddf0cc65e8809e93f
Harold Camacho Herrera <haroldherrera2001@outlook.com>
2022-11-24 22:54:22 +0000

    Creacion de Proyecto Java

---
+++ JavaX/src/App/App.java
@@ -0,0 +1,39 @@
+package App;
+import Day1.*;
+
+/*
+ * Esta class se utiliza para Ejecutar lo que se haga en el curso
+ * tendrás que crear un objeto de de la class que veamos en clase 
+ * y utiliza sus metodos para corroborar lo aprendido.
+ */
+public class App{
+
+    public static void main(String[] args) {
+        Clase01.hola();
+    }
+
+
+
+
+    /*
+    public static void tipos_de_datos(){
+        
+        //Datos numericos enteros
+        byte  numero1;  //Default = 0       [-128 to 127]
+        short numero2;  //Default = 0       [-32,768 to 32,767]
+        int   numero3;  //Default = 0       [-2,147,483,648 to 2,147,483,647]
+        long  numero4;  //Default = 0L      [-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807]
+
+        //Datos numericos reales
+        float  numero5; //Default = 0.0f    [????]
+        double numero6; //Default = 0.0d    [????]
+
+        //Datos booleanos
+        boolean valor;  //Default = false   [true or false]
+
+        //Datos de caracteres
+        char caracter;  //Default = \u0000  [Un solo caracter ya sea numerico o simbolico]
+        String  chars;  //Default = null    [Caracteres unidos o arreglo de char-s]
+    }
+    */
+}
